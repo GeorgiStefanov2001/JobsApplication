2a375108d3a8e0d93ac98e9b66f8848687971938
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-04-04 16:16:16 +0300

    Fixed the regexes (probably?)

--- JobApplication/JobApplication.ViewModels/CreateProjectViewModel.cs
+++ JobApplication/JobApplication.ViewModels/CreateProjectViewModel.cs
@@ -14,22 +14,22 @@ namespace JobApplication.ViewModels
 
         [Display(Name = "Technology")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter technology")]
-        [RegularExpression(@"^[a-zA-Z''-'\s]{1,30}$", ErrorMessage = "A technology should only contain letters")]
+        [RegularExpression("[A-Za-z][A-Za-z0-9.,;/?_\\s*]{5,200}", ErrorMessage = "Technologies should not contain only numbers and must be between 5 and 100 characters")]
         public string Technology { get; set; }
 
         [Display(Name = "Description")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter a description")]
-        [RegularExpression("[A-Za-z][A-Za-z0-9._\\s*]{10,200}", ErrorMessage = "A description should not contain only numbers and must be between 10 and 200 characters")]
+        [RegularExpression("[A-Za-z][A-Za-z0-9.,;/?_\\s*]{10,200}", ErrorMessage = "A description should not contain only numbers and must be between 10 and 200 characters")]
         public string Description { get; set; }
 
         [Display(Name = "Achieved goals")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter achieved goals")]
-        [RegularExpression("[A-Za-z][A-Za-z0-9._\\s*]{5,200}", ErrorMessage = "This field should not contain only numbers and must be between 5 and 200 characters")]
+        [RegularExpression("[A-Za-z][A-Za-z0-9.,/?;_\\s*]{5,200}", ErrorMessage = "This field should not contain only numbers and must be between 5 and 200 characters")]
         public string AchievedGoals { get; set; }
 
         [Display(Name = "Future goals")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter your future goals")]
-        [RegularExpression("[A-Za-z][A-Za-z0-9._\\s*]{5,200}", ErrorMessage = "This field should not contain only numbers and must be between 5 and 200 characters")]
+        [RegularExpression("[A-Za-z][A-Za-z0-9.,/?;_\\s*]{5,200}", ErrorMessage = "This field should not contain only numbers and must be between 5 and 200 characters")]
         public string FutureGoals { get; set; }
     }
 }
