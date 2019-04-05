02199d5cdda05ebafc07aaafe5a0f6957b5d2ece
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-04-05 16:38:02 +0300

    Fixed some spelling mistakes

--- JobApplication/JobApplication.Data/ConfigurationData.cs
+++ JobApplication/JobApplication.Data/ConfigurationData.cs
@@ -3,11 +3,11 @@
 namespace JobApplication.Data
 {
     /// <summary>
-    /// This class holds the connection string for connectiong to the data base
+    /// This class holds the connection string for connecting to the data base
     /// </summary>
     public static class ConfigurationData
     {
-        //public const string ConnectionString = "Server=GAS-LAPTOP; Database=JobApp; Integrated Security=true";
-        public const string ConnectionString = "Server=DESKTOP-178HOMD; Database=JobApp; Integrated Security=true";
+        public const string ConnectionString = "Server=GAS-LAPTOP; Database=JobApp; Integrated Security=true";
+        //public const string ConnectionString = "Server=DESKTOP-178HOMD; Database=JobApp; Integrated Security=true";
     }
 }
